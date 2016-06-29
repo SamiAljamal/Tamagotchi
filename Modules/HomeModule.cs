6b319a47fab43de06bd0a9e3b82d9c3d555271e3
Sami Al-Jamal and Stewart Cole <sami+stewart@>
2016-06-29 16:36:38 -0700

    semi-working tamagotchi.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,11 +9,34 @@ namespace Tamagotchi
     public HomeModule()
     {
       Get["/"] = _ => View["create_new_tamagotchi.cshtml"];
-      Post["/Tamagotchi"] = _ => {
+      Post["/tamagotchi"] = _ => {
         string name = Request.Form["tamaName"];
-        Tama tama = new Tama(name);
+        Tama tama = new Tama();
+        tama.SetName(name);
         return View["tamagotchi_status.cshtml", tama];
       };
+      Post["/sleep"] = _ => {
+        Tama currentTama =  Tama.CurrentTama();
+        currentTama.Sleep();
+        return View["tamagotchi_status.cshtml",currentTama];
+      };
+      Post["/eat"] = _ => {
+        Tama currentTama =  Tama.CurrentTama();
+        currentTama.Eat();
+        return View["tamagotchi_status.cshtml",currentTama];
+      };
+      Post["/clean"] = _ => {
+      Tama currentTama =  Tama.CurrentTama();
+      currentTama.Clean();
+        return View["tamagotchi_status.cshtml",currentTama];
+      };
+      Post["/play"] = _ => {
+        Tama currentTama =  Tama.CurrentTama();
+        currentTama.Play();
+        return View["tamagotchi_status.cshtml",currentTama];
+      };
+
+
     }
   }
 }
--- Objects/TamaObjects.cs
+++ Objects/TamaObjects.cs
@@ -10,18 +10,28 @@ namespace Tamagotchi.Objects
     private int _happiness;
     private int _tiredness;
     private int _cleanliness;
+    private static Tama _currentTama = new Tama();
+    private bool _isAlive;
 
     private int _condition;
 
-    public Tama(string name)
+    public Tama()
     {
-      _name = name;
+      _name = "";
       _hunger = 0;
       _happiness = 50;
       _cleanliness = 100;
       _tiredness = 0;
       _condition = 100;
+      _currentTama = this;
+      _isAlive = true;
     }
+
+    public static Tama CurrentTama()
+    {
+      return _currentTama;
+    }
+
 
     public string GetName()
     {
@@ -72,12 +82,41 @@ namespace Tamagotchi.Objects
       _condition = newCondition;
     }
 
+
     public void Sleep()
     {
       this._hunger += 40;
-      this._tiredness = 0;
-      this._happiness += 2;
-      this._cleanliness -= 50;
+      this._tiredness -= 50;
+      this._happiness -= 10;
+      this._cleanliness -= 30;
+    }
+    public void Eat()
+    {
+      this._hunger -= 40;
+      this._cleanliness -= 20;
+      this._tiredness += 20;
+    }
+    public void Clean()
+    {
+      this._hunger += 20;
+      this._happiness -= 10;
+      this._cleanliness = 100;
+      this._tiredness += 20;
+    }
+    public void Play()
+    {
+      this._hunger += 20;
+      this._happiness += 30;
+      this._cleanliness -= 30;
+      this._tiredness += 40;
+    }
+
+    public void Death()
+    {
+      if (this._hunger >=100 || this._happiness <= 0 || this._cleanliness <= 0 || this._tiredness >= 100)
+      {
+        _isAlive = false;
+      }
     }
 
   }
